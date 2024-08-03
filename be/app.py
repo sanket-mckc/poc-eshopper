514b2cab5a5411d41d0c02bcd56cde7e07fd52c3
shivamMahajan09 <shivam_mahajan@mckinsey.com>
2024-08-03 16:50:35 +0530

    chore: updated message context

--- be/app.py
+++ be/app.py
@@ -11,13 +11,19 @@ openai_base_url = os.environ['OPENAI_BASE_URL']
 # Set your OpenAI API key here
 client = openai.OpenAI(api_key=openai_api_key, base_url=openai_base_url)
 
-def get_sales_assistant_response(user_input):
+def get_sales_assistant_response(user_input, old_message=None):
+    start_prompt =  {"role": "system", "content": "As a seasoned furniture salesman with over 10 years of experience, greet the customer warmly and ask detailed questions about their space, style preferences, functional needs, budget to suggest the best possible options. I want you to ask me these questions one at a time making sure number of questions in total don't exceed more than 3 and should create a final confirmation message with all the requirements and make me confirm on it."}
+    if old_message:
+        messages = [start_prompt]+ old_message + [user_input]
+    else:
+        messages = [
+            start_prompt,
+            user_input,
+        ]
+    
     response = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "As a seasoned furniture salesman with over 10 years of experience, greet the customer warmly and ask detailed questions about their space, style preferences, existing furniture, functional needs, budget, and timeframe to suggest the best possible options."},
-            {"role": "user", "content": user_input},
-        ]
+        messages=messages
     )
     return response
 
@@ -26,8 +32,9 @@ def sales_assistant():
     user_input = request.json.get('input')
     if not user_input:
         return jsonify({"error": "No input provided"}), 400
+    old_message = request.json.get('old_message')
     
-    response = get_sales_assistant_response(user_input)
+    response = get_sales_assistant_response(user_input, old_message)
     return response.to_json()   
 
 if __name__ == '__main__':
